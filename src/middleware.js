3dfc1798af0f0abd3f73d80671baa2ffdf973600
JAbelP <abelpinales97@gmail.com>
2024-03-03 04:29:29 -0500

    Middleware works >:)

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,37 @@
-let locales = ["en", "es","pt"];
-
+let locales = ["en", "es", "pt"];
+import { NextResponse } from "next/server";
 // Get the preferred locale, similar to the above or using a library
 function getLocale(request) {
   return request.cookies.get("NEXT_LOCALE").value;
 }
 
 export function middleware(request) {
-  // request.cookies.set("NEXT_LOCALE", "es");
-  // console.log("in line 11", request.cookies.get("NEXT_LOCALE").value);
-
-  // Check if there is any supported locale in the pathname
+  //extract the pathname
+  //http://localhost:3000/en/en/QuienesSomos -> /en/en/QuienesSomos (No Host Name)
   const { pathname } = request.nextUrl;
-  console.log("in line 14", pathname);
+
+  //basically an inverse forloop, does it
+  //have at least one instance of this happening return true
   const pathnameHasLocale = locales.some(
+    //do any of the
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
   if (pathnameHasLocale) {
-    const locale = pathname.split("/")[1];
+    //if this is true then you need to remove whatever the heck it is fromo spaces 3-6
+    const doubleLanguage = ["en/", "es/", "pt/"].some(
+      (slash) => pathname.substring(3, 7).split(`${slash}`).length >= 2
+    );
+    //we found a duplicate
+    if (doubleLanguage) {
+      console.log("languge substring", pathname.substring(4, 7));
+      console.log(" Try to slice this");
+      console.log("request URL", request.url);
+      const retunUrl = pathname.slice(0, 4) + pathname.slice(7);
+      const url = request.nextUrl.clone().origin + retunUrl;
+      console.log("url", url);
+      return NextResponse.redirect(url);
+    }
     request.cookies.set("NEXT_LOCALE");
     return;
   }
