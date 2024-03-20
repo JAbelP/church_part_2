50dfdab030e4ef5c01c033c3c6033a7d314ff9e9
JAbelP <abelpinales97@gmail.com>
2024-03-20 14:37:46 -0400

    StackOverflow Resolution

--- src/app/[lang]/api/submit/route.js
+++ src/app/[lang]/api/submit/route.js
@@ -1,65 +1,104 @@
-// [IMPROVE] :  try implementing this https://www.youtube.com/watch?v=dDpZfOQBMaU&ab_channel=LeeRobinson
+// import nodemailer from "nodemailer";
+// import { NextResponse } from "next/server";
 
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   console.log("handler")
-//   const data = req.body
-//   const id = await (data)
-//   console.log(id)
-//   res.status(200).json({ id })
+// async function sendMail(body) {
+//   const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
+//   const transport = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: SMTP_EMAIL,
+//       pass: SMTP_PASSWORD,
+//     },
+//   });
+
+//   try {
+//     const testResult = await transport.verify();
+//   } catch (error) {
+//     console.error({ error });
+//     return;
+//   }
+//   const bodyJsonString = JSON.stringify(body);
+//   let sendResult;
+//   try {
+//     if (body.subject === "New Member") {
+//       sendResult = await transport.sendMail({
+//         from: SMTP_EMAIL,
+//         to: SMTP_EMAIL,
+//         subject: "New Member",
+//         text: JSON.stringify(body),
+//         // html: body,
+//       });
+//     }
+//     if (body.subject === "Ofrenda") {
+//       sendResult = await transport.sendMail({
+//         from: SMTP_EMAIL,
+//         to: SMTP_EMAIL,
+//         subject: "New Member",
+//         text: JSON.stringify(body),
+//       });
+//       return {};
+//     }
+//     console.log(sendResult);
+//   } catch (error) {
+//     console.log(error);
+//   }
 // }
 
-import nodemailer from "nodemailer";
-import { NextResponse } from "next/server";
+// export async function POST(request) {
+//   const body = await request.json();
+//   await sendMail(body);
+//   return NextResponse.json({ status: 200 });
+// }
 
-async function sendMail(body) {
+const nodemailer = require("nodemailer");
+
+export default async (req, res) => {
   const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
-  const transport = nodemailer.createTransport({
-    service: "gmail",
+
+  // const { firstName, lastName, email, message } = JSON.parse(req.body);
+
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
     auth: {
       user: SMTP_EMAIL,
       pass: SMTP_PASSWORD,
     },
+    secure: true,
   });
 
-  try {
-    const testResult = await transport.verify();
-  } catch (error) {
-    console.error({ error });
-    return;
-  }
-  const bodyJsonString = JSON.stringify(body);
-  let sendResult;
-  try {
-    if (body.subject === "New Member") {
-      sendResult = await transport.sendMail({
-        from: SMTP_EMAIL,
-        to: SMTP_EMAIL,
-        subject: "New Member",
-        text: JSON.stringify(body),
-        // html: body,
-      });
-    }
-    if (body.subject === "Ofrenda") {
-      sendResult = await transport.sendMail({
-        from: SMTP_EMAIL,
-        to: SMTP_EMAIL,
-        subject: "New Member",
-        text: JSON.stringify(body),
-      });
-      return {};
-    }
-    console.log(sendResult);
-  } catch (error) {
-    console.log(error);
-  }
-}
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
 
-export async function POST(request) {
-  const body = await request.json();
-  sendMail(body);
-  return NextResponse.json({ status: 200 });
-}
+  const mailData = {
+    from: SMTP_EMAIL,
+    to: SMTP_EMAIL,
+    subject: "New Member",
+    text: "Test",
+  };
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+
+  res.status(200).json({ status: "OK" });
+};
