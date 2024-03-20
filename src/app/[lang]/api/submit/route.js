40100c2327d79d7f47a25dc04bdd35e1e6ef214e
JAbelP <abelpinales97@gmail.com>
2024-03-20 15:02:03 -0400

    Making it a promise

--- src/app/[lang]/api/submit/route.js
+++ src/app/[lang]/api/submit/route.js
@@ -1,104 +1,56 @@
-// import nodemailer from "nodemailer";
-// import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import { NextResponse } from "next/server";
 
-// async function sendMail(body) {
-//   const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
-//   const transport = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: SMTP_EMAIL,
-//       pass: SMTP_PASSWORD,
-//     },
-//   });
-
-//   try {
-//     const testResult = await transport.verify();
-//   } catch (error) {
-//     console.error({ error });
-//     return;
-//   }
-//   const bodyJsonString = JSON.stringify(body);
-//   let sendResult;
-//   try {
-//     if (body.subject === "New Member") {
-//       sendResult = await transport.sendMail({
-//         from: SMTP_EMAIL,
-//         to: SMTP_EMAIL,
-//         subject: "New Member",
-//         text: JSON.stringify(body),
-//         // html: body,
-//       });
-//     }
-//     if (body.subject === "Ofrenda") {
-//       sendResult = await transport.sendMail({
-//         from: SMTP_EMAIL,
-//         to: SMTP_EMAIL,
-//         subject: "New Member",
-//         text: JSON.stringify(body),
-//       });
-//       return {};
-//     }
-//     console.log(sendResult);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// export async function POST(request) {
-//   const body = await request.json();
-//   await sendMail(body);
-//   return NextResponse.json({ status: 200 });
-// }
-
-const nodemailer = require("nodemailer");
-
-export default async (req, res) => {
+async function sendMail(body) {
   const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
 
-  // const { firstName, lastName, email, message } = JSON.parse(req.body);
-
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
+  const transport = nodemailer.createTransport({
+    service: "gmail",
     auth: {
       user: SMTP_EMAIL,
       pass: SMTP_PASSWORD,
     },
-    secure: true,
   });
 
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("Server is ready to take our messages");
-        resolve(success);
-      }
-    });
-  });
+  try {
+    const testResult = await transport.verify();
+  } catch (error) {
+    console.error({ error });
+    return;
+  }
+  const bodyJsonString = JSON.stringify(body);
+  let sendResult;
+  try {
+    if (body.subject === "New Member") {
+      await new Promise((resolve, reject) => {
+        sendResult = transport.sendMail({
+          from: SMTP_EMAIL,
+          to: SMTP_EMAIL,
+          subject: "New Member",
+          text: JSON.stringify(body),
+          // html: body,
+        });
+      });
+    }
+    if (body.subject === "Ofrenda") {
+      await new Promise((resolve, reject) => {
+        sendResult = transport.sendMail({
+          from: SMTP_EMAIL,
+          to: SMTP_EMAIL,
+          subject: "Ofrenda",
+          text: JSON.stringify(body),
+          // html: body,
+        });
+      });
+    }
+    console.log(sendResult);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  const mailData = {
-    from: SMTP_EMAIL,
-    to: SMTP_EMAIL,
-    subject: "New Member",
-    text: "Test",
-  };
-
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailData, (err, info) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
-    });
-  });
-
-  res.status(200).json({ status: "OK" });
-};
+export async function POST(request) {
+  const body = await request.json();
+  await sendMail(body);
+  return NextResponse.json({ status: 200 });
+}
